f150ec4d51ad04a03696ad306184523c5db8ab83
taribogeorge <taribogeorge9@gmail.com>
2022-10-19 23:20:11 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,6 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-
 		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
@@ -21,6 +20,7 @@ void times_table(void)
 
 			if (prod <= 9)
 				_putchar(' ');
+
 			else
 				_putchar((prod / 10) + '0');
 
@@ -28,3 +28,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
+}
